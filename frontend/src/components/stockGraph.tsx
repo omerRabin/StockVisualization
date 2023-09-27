e08cb4ad4908c2cc55faea8a314788a3ab31c748
omerRabin <73130830+omerRabin@users.noreply.github.com>
2023-09-27 19:38:42 +0300

    connect the data to graph

--- frontend/src/components/stockGraph.tsx
+++ frontend/src/components/stockGraph.tsx
@@ -2,68 +2,75 @@ import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Ranges } from '../enums';
 import { BasicLineChart } from './';
-import { historicalStockData } from '../types';
-import { useQuery } from 'react-query';
 import '../styles/stockGraph.css';
 import axios from 'axios';
+import { useQuery, useQueryClient } from 'react-query';
 
 const SERVER_API = 'http://localhost:4567/api';
 
-const getStockData = async (symbol: string, range: Ranges) => {
-  const response = await axios.get(`${SERVER_API}/data/graph?symbol=${symbol}&range=${range}`);
-  return response.data as historicalStockData[];
+const rangeOptions = [
+  { value: Ranges.DAY, label: '1 Day' },
+  { value: Ranges.WEEK, label: '1 Week' },
+  { value: Ranges.MONTH, label: '1 Month' },
+  { value: Ranges.QUARTER_YEAR, label: '3 Month' },
+  { value: Ranges.HALF_YEAR, label: '6 Month' },
+  { value: Ranges.YEAR, label: '1 Year' },
+];
+
+const fetchStockData = async (symbol: string, selectedRange: Ranges) => {
+  const response = await axios.get(`${SERVER_API}/data/graph?symbol=${symbol}&range=${selectedRange}`);
+  return response.data;
 };
 
 const StockGraph = () => {
   const { symbol = '' } = useParams();
-  const [range, setRange] = useState(Ranges.WEEK);
+  const [selectedRange, setSelectedRange] = useState<Ranges>(Ranges.WEEK); // Specify the type of selectedRange
 
-  const { data: stockData = [], isLoading } = useQuery(
-    ['stockData', symbol, range],
-    () => getStockData(symbol, range),
-    { enabled: false }
-  );
+  const labels = ['Label 1', 'Label 2', 'Label 3', 'Label 4', 'Label 5'];
 
-  const labels = ['Label 1', 'Label 2', 'Label 3', 'Label 4', 'Label5'];
+  const queryClient = useQueryClient(); // Get the query client
+
+  const {
+    data: stockData,
+    isLoading,
+    isError,
+  } = useQuery(['stockData', symbol, selectedRange], () => fetchStockData(symbol, selectedRange), {
+    initialData: queryClient.getQueryData(['stockData', symbol, selectedRange]),
+  });
 
   const handleRangeChange = (newRange: Ranges) => {
-    setRange(newRange);
+    // Specify the type of newRange
+    setSelectedRange(newRange);
+    queryClient.invalidateQueries(['stockData', symbol, newRange]);
   };
 
   return (
     <div className='temp-column'>
       {isLoading ? (
         <p>Loading...</p>
+      ) : isError ? (
+        <p>Error fetching stock data</p>
       ) : (
         <div className='stock-graph-container'>
           <h2 className='stock-graph-title'>{symbol} Stock Graph</h2>
           {stockData && (
             <BasicLineChart
-              data={[1, 2, 3]} // Replace with your stockData
+              data={stockData.candlesClosedPricesList}
               labels={labels}
               datasetLabel={`${symbol} Data`}
               canvasBackgroundColor='black'
             />
           )}
-          <div className='range-selection-containter'>
-            <button className='range-selection' onClick={() => handleRangeChange(Ranges.DAY)}>
-              1 Day
-            </button>
-            <button className='range-selection' onClick={() => handleRangeChange(Ranges.WEEK)}>
-              1 Week
-            </button>
-            <button className='range-selection' onClick={() => handleRangeChange(Ranges.MONTH)}>
-              1 Month
-            </button>
-            <button className='range-selection' onClick={() => handleRangeChange(Ranges.QUARTER_YEAR)}>
-              3 Month
-            </button>
-            <button className='range-selection' onClick={() => handleRangeChange(Ranges.HALF_YEAR)}>
-              6 Month
-            </button>
-            <button className='range-selection' onClick={() => handleRangeChange(Ranges.YEAR)}>
-              1 Year
-            </button>
+          <div className='range-selection-container'>
+            {rangeOptions.map((option) => (
+              <button
+                key={option.value}
+                className={`range-selection ${selectedRange === option.value ? 'selected' : ''}`}
+                onClick={() => handleRangeChange(option.value)}
+              >
+                {option.label}
+              </button>
+            ))}
           </div>
         </div>
       )}
