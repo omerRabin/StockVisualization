8a44e36b06fd49c547c6f8b3f4e512ca06757dff
omerRabin <73130830+omerRabin@users.noreply.github.com>
2023-10-20 16:51:18 +0300

    trying to add socket.io to client-side

--- backend/server.ts
+++ backend/server.ts
@@ -5,7 +5,6 @@ import http from 'http';
 import { Server } from 'socket.io';
 import { fetchRealTimeStockData } from './src/sockets';
 
-
 require('dotenv').config();
 
 const app = express();
@@ -15,20 +14,22 @@ const port = process.env.PORT || 4567;
 app.use(mainRouter);
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  path: '/socket.io',
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
 
 io.on('connection', (socket) => {
-    console.log('A client connected');
+  console.log('A client connected');
 
-    socket.on('realTimeStockData', (symbol) => {
-        fetchRealTimeStockData(io, symbol);
-    });
+  socket.on('realTimeStockData', (symbol) => {
+    fetchRealTimeStockData(io, symbol);
+  });
 
-    socket.on('disconnect', () => {
-        console.log('A client disconnected');
-    });
+  socket.on('disconnect', () => {
+    console.log('A client disconnected');
+  });
 });
